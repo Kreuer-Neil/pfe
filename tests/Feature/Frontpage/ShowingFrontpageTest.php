d0b2d72e36e11c6346de00cf38460dd7d7363826
Kreuer-Neil <neil.kreuer@student.hepl.be>
2026-03-17 17:29:38 +0100

    Added base tests

---
+++ tests/Feature/Frontpage/ShowingFrontpageTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use App\Enums\Languages;
+use App\Models\User;
+
+test('landing pages can be accessed and contain translated strings', function () {
+
+    /*foreach (Languages::cases() as $locale) {
+        Lang::setLocale($locale->value);*/
+
+        $requestHomepage = $this->get(route('frontpage.index'));
+        $requestPolicyPage = $this->get(route('frontpage.policy'));
+
+        $requestHomepage->assertStatus(200);
+        $requestHomepage->assertSee(__('frontpage.home.header.title'));
+
+        $requestPolicyPage->assertStatus(200);
+//    }
+});
+
+
+test('contextual login/open app button changes accordingly', function () {
+    $user = User::factory()->create();
+
+    $request1 = $this->get(route('frontpage.index'));
+
+    $request1->assertSee(__('frontpage.nav.login'));
+
+    $this->actingAs($this->user = User::factory()->create());
+    $request2 = $this->get(route('frontpage.index'));
+
+    $request2->assertSee(__('frontpage.nav.app'));
+});
