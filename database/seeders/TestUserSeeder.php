6954a4a3277c331788b13d9b51bb6749afaff433
Kreuer-Neil <neil.kreuer@student.hepl.be>
2026-05-05 12:30:35 +0200

    Updated seeder with more interesting data during tests

--- database/seeders/TestUserSeeder.php
+++ database/seeders/TestUserSeeder.php
@@ -14,7 +14,7 @@ class TestUserSeeder
     /**
      * Seed the Test User
      */
-    public static function run()
+    public static function run(): void
     {
         $testUser = User::firstOrCreate(
             ['email' => 'test@example.com'],
@@ -28,12 +28,16 @@ class TestUserSeeder
             ]
         );
 
+        $projectUsers = User::factory(5)->create(
+            ['email_verified_at' => now(),]
+        );
+
         $sharedGardenProject = Project::create(
             [
                 'owner_id' => $testUser->id,
                 'name' => 'Sunshine Alley 22b',
-                'description' => 'Building 22',
-                'slug' => 'Building 22',
+                'description' => 'The group for Building 22b on Sunshine Alley. Our shared garden is soon to become a vegetable garden, and we\'re workin hard on it. So that we could dine together sometimes, nearly for free. Living already costs too much, at least mutual aid is free.',
+                'slug' => 'buil22',
 
                 'is_private' => true,
             ]
@@ -45,10 +49,18 @@ class TestUserSeeder
             'role' => ProjectRole::ADMIN,
         ]);
 
+        foreach ($projectUsers as $user) {
+            Member::create([
+                'project_id' => $sharedGardenProject->id,
+                'user_id' => $user->id,
+                'role' => (!random_int(0,1)) ? ProjectRole::MEMBER : ProjectRole::TASK_MANAGER,
+            ]);
+        }
+
         $sharedGardenProject->addTask(new Task([
             'title' => 'Water the plants',
             'user_id' => $testUser->id,
-            'description' => 'The plants in our appartment’s shared garden need constant watering and care, especially if it didn’t rain.',
+            'description' => 'The plants in our appartment\'s shared garden need constant watering and care, especially if it didn\'t rain.',
             'min_participations' => 8,
             'due_at' => Carbon::createFromDate('2026', '06', '23')
         ]), $testUser)->participate($testUser);
@@ -58,6 +70,9 @@ class TestUserSeeder
             'project_id' => $sharedGardenProject->id,
         ]) as $task) {
             $task->participate($testUser);
+            $task->participate($projectUsers->random(1)->first());
+            $task->participate($projectUsers->random(1)->first());
+            $task->participate($projectUsers->random(1)->first());
         }
     }
 }
