aa2df58741922961b59d13d03320ea6591fe19e8
Kreuer-Neil <neil.kreuer@student.hepl.be>
2026-05-13 21:12:29 +0200

    Trying to fix modal

--- resources/js/components/tasks/task-display.tsx
+++ resources/js/components/tasks/task-display.tsx
@@ -1,13 +1,18 @@
 import {IProject, ITask, ITaskMiniature} from "@/types";
 // import {agenda} from '@/routes';
 import TaskItem from "@/components/tasks/task-item";
-import { ReactNode, useState} from "react";
+import {ReactNode, useState} from "react";
 import {cn} from "@/lib/utils";
-import {ClipboardPlus} from "lucide-react";
-import {laravelDateToJsDate,} from "@/helpers/date";
+import {CalendarClock, ClipboardPlus} from "lucide-react";
+import {laravelDateToJsDate, upcomingDateToString,} from "@/helpers/date";
 import ShowMore from "@/components/buttons/show-more";
 import IconButton from "@/components/buttons/icon-button";
 import {useTranslation} from "react-i18next";
+import ModalCast from "@/components/modals/modal-cast";
+import ModalSection from "@/components/modals/modal-section";
+import ProjectIcon from "@/components/icons/project-icon";
+import ReactModal from "react-modal";
+import {show as tasksShow} from "@/actions/App/Http/Controllers/TaskController";
 
 
 interface TaskDisplayProps {
@@ -20,11 +25,11 @@ interface TaskDisplayProps {
     maxLength?: bigint | null;
 }
 
-function TasksList({tasks, projectContext, maxLength, /*onTapTask*/}: {
+function TasksList({tasks, projectContext, maxLength, onTapTask}: {
     tasks: ITaskMiniature[];
     projectContext: boolean;
     maxLength: bigint;
-    // onTapTask: (id: string) => void;
+    onTapTask: (id: string) => void;
 }): ReactNode {
     const {t} = useTranslation('date');
     const length = tasks.length;
@@ -38,8 +43,8 @@ function TasksList({tasks, projectContext, maxLength, /*onTapTask*/}: {
             return (
                 <li className="w-full flex flex-col gap-4" key={task.id}>
                     {precedentMonthCondition ? <span className="month-divider">{t('month_' + month)}</span> : ''}
-                    <TaskItem taskMiniature={task} isInProjectPage={projectContext}
-                              // setModalTaskId={onTapTask}
+                    <TaskItem task={task} isInProjectPage={projectContext}
+                              onTap={onTapTask}
                     />
                 </li>
             );
@@ -62,7 +67,7 @@ export default function TaskDisplay(
         className = '',
         project = null,
         maxLength = 3n,
-    }: TaskDisplayProps,): ReactNode {
+    }: TaskDisplayProps): ReactNode {
     const {t} = useTranslation('projects');
     const [maxItemsLength, setMaxItemsLength] = useState(maxLength);
     // const [showMoreIcon, setShowMoreIcon] = useState(LucideChevronDown);
@@ -77,7 +82,33 @@ export default function TaskDisplay(
             setMaxItemsLength(maxLength! * 2n);
             setShowMoreState(false);
         }
+    }
 
+    const [modalTask, setModalTask] = useState<ITask>();
+    const [showModal, setShowModal] = useState<boolean>(false);
+
+    function onTaskTap(id: string) {
+        // TODO find how to optimize this (and avoid multiple code reception)
+        const fetchTask = async (): Promise<ITask | undefined> => {
+            try {
+                const params = new URLSearchParams();
+                params.append('task_id', id);
+
+                const response = await fetch(`${tasksShow(id).url}?${params}`);
+                const data: { task: ITask } = await response.json();
+                return (data.task);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchTask().then((modalTask: ITask | undefined) => {
+            if (!modalTask) {
+                //     setShowErrorModal(true);
+                return;
+            }
+            setModalTask(modalTask);
+            setShowModal(true);
+        });
     }
 
     /*useEffect(() => {
@@ -117,7 +148,7 @@ export default function TaskDisplay(
                 tasks.length > 0
                     // Task items
                     ? <TasksList tasks={tasks} projectContext={(project != null)} maxLength={maxItemsLength!}
-                                 // onTapTask={onTaskTap}
+                        onTapTask={onTaskTap}
                     />
                     : <div className="thumbnails-list-container"><p>{t('task_empty_message')}</p></div>
             }
@@ -125,6 +156,35 @@ export default function TaskDisplay(
                 <ShowMore showMore={showMoreState} onClick={onShowMore}/>
                 {/*<ButtonText href={agenda().url} textContent={actionText ?? t('task.show_agenda')} icon={LucideCalendarDays}/>*/}
             </div>
+            <ReactModal isOpen={showModal} appElement={document.querySelector('body')!} className="mx-2 my-3 max-w-3xl"
+                        style={{overlay: {backgroundColor: 'var(--color-modal-behind)'},}}>
+                <ModalCast title={modalTask ? modalTask.title : ''} closeModal={() => {
+                    setShowModal(false)
+                }}>
+
+                    <ModalSection className="border-none">
+                        <div className="flex gap-1">
+                            <ProjectIcon
+                                project={modalTask ? modalTask.project : {name: '', icon: '', slug: '', id: ''}}
+                                size="small"/>
+                            <p className="item-title">{modalTask ? modalTask.project.name : ''}</p>
+                        </div>
+                        {modalTask ? modalTask.due_at ?
+                            <p className="flex gap-1">
+                                <CalendarClock/>{upcomingDateToString(laravelDateToJsDate(modalTask ? modalTask.due_at : ''))}
+                            </p> : '' : ''
+                        }
+                        <p className="mt-1">
+                            {modalTask ? modalTask.description : ''}
+                        </p>
+                    </ModalSection>
+
+                    {/* Modal section */}
+                    <ModalSection>
+                        test
+                    </ModalSection>
+                </ModalCast>
+            </ReactModal>
         </section>
     );
 }
