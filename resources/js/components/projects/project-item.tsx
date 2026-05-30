c98a706a9e3f4c8887757455864e1a5bc9ad8e4d
Kreuer-Neil <neil.kreuer@student.hepl.be>
2026-05-30 12:36:26 +0200

    Accessibility quick fix

--- resources/js/components/projects/project-item.tsx
+++ resources/js/components/projects/project-item.tsx
@@ -13,38 +13,39 @@ export default function ProjectItem({project}: ProjectItemsProps) {
     const place: string | null = project.place;
     const coordinates: string | null = project.coordinates;
     return (
-        <Link as={'article'} tabIndex={0} href={projectsShow(project.slug)} className="thumbnail-item"
-              onKeyDown={(e)=>{
-                  // TODO force navigation
-                  if (e.key === '13' || e.key === ' ') (projectsShow(project.slug));
-              }}>
-            <div className={'flex gap-1 items-center'}>
-                <ProjectIcon project={project}/>
-                <h3 className="item-title w-full">{project.name}</h3>
-            </div>
-            <p>{project.description}</p>
-            {
-                place && coordinates ?
-                    // Google maps link
-                    // TODO fix link not clickable because of link nav
-                    <a href={'https://www.google.com/maps/@' + coordinates.replace(' ', '') +',14z?entry=ttu&g_ep=EgoyMDI2MDUwMi4wIKXMDSoASAFQAw%3D%3D'}
-                       className="flex gap-1">
-                        <MapPin/>
-                        <p>{place}</p>
-                    </a> : ''
-            }
-            <div className="flex flex-wrap gap-1 gap-y-0.5">
-                {/* TODO what's new since last passage on project */}
-                <div className="flex gap-1 ml-auto">
-                    {/*Related users*/}
-                    <div className="item-tag">
-                        {project.members_count}
-                        <UsersRound/>
-                    </div>
-                    {project.is_member ? <UserRoundCheck className="item-tag bg-tag-valid"/> : ''}
-                    {/*Other infos*/}
+        <Link as={'a'} tabIndex={0} href={projectsShow(project.slug)}>
+            <article className="thumbnail-item">
+                <div className={'flex gap-1 items-center'}>
+                    <ProjectIcon project={project}/>
+                    <h3 className="item-title w-full">{project.name}</h3>
                 </div>
-            </div>
+                <p>{project.description}</p>
+                {
+                    place && coordinates ?
+                        // Google maps link
+                        // TODO fix link not clickable because of link nav
+                        // <a href={'https://www.google.com/maps/@' + coordinates.replace(' ', '') + ',14z?entry=ttu&g_ep=EgoyMDI2MDUwMi4wIKXMDSoASAFQAw%3D%3D'}
+                        <div
+                            className="flex gap-1">
+                            <MapPin/>
+                            <p>{place}</p>
+                        </div>
+                        // </a>
+                    : ''
+                }
+                <div className="flex flex-wrap gap-1 gap-y-0.5">
+                    {/* TODO what's new since last passage on project */}
+                    <div className="flex gap-1 ml-auto">
+                        {/*Related users*/}
+                        <div className="item-tag">
+                            {project.members_count}
+                            <UsersRound/>
+                        </div>
+                        {project.is_member ? <UserRoundCheck className="item-tag bg-tag-valid"/> : ''}
+                        {/*Other infos*/}
+                    </div>
+                </div>
+            </article>
         </Link>
     );
 }
